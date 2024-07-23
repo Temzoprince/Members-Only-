fdb213c952866b4af38a00c0b8c381c3e31d9b50
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-07-23 22:51:19 +0100

    Complete solution

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to new_post_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
